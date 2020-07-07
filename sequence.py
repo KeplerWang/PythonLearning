76daae1765a3f61ace716d8d86345b06f06298d4
KeplerWang <1692705146@qq.com>
2020-07-07 20:04:54 +0800

    update

--- sequence.py
+++ sequence.py
@@ -33,12 +33,14 @@ list1.append(40)
 print(list1[-1])    # append() 将元素加在最后
 
 list1.insert(1, 16)
-print(list1)        # insert(1, 16) 在索引 1 处加一个 16
+print(list1)        # insert(1, 16) 在索引 1 处插入一个 16
 
 list1.sort()        # 从小到大排序
 # list1.sort(reverse=True)    # 从大到小排序
 print(list1)        # sort() 将list排序
 
+del(list1[1])        # 删除 index=1 的元素
+print(list1)
 
 # 二维列表
 multi_dim_list = [[1, 2],
@@ -50,7 +52,31 @@ print(multi_dim_list[1][1])
 # 字典 dictionary
 # 1.用 {} 包围
 # 2.字典中有 key 和 value 两种元素，key 和 value 可以是字符串/数字
+# 3.字典是一个无序的容器
 dic1 = {'apple': 1, 'banana': 2, 'pineapple': 3}
+
 print(dic1['apple'])
+dic1['banana'] = 22  # 修改
+dic1['pear'] = 4     # 增加
+print(dic1)
+
+del(dic1['banana'])  # 删除 key='banana' 的元素
+print(dic1)
+
+for key in dic1.keys():     # 通过 key 来遍历
+    print(key, dic1[key])
 
 
+def func():
+    print('func()')
+
+
+# Sequence 组合
+dic2 = {1: [1, 2, 3],
+        2: [4, 5, 6],
+        3: [7, 8, 9],
+        'orange': func}
+print(dic2)
+print(dic2[2])
+print(dic2[2][2])
+dic2['orange']()    # 这时存的是函数的地址 加 () 后实现调用
