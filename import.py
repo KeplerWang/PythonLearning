ba136b90300f42b1cb7b0212ff8cb680cef34601
KeplerWang <1692705146@qq.com>
2020-07-08 15:28:58 +0800

    update import.py

--- import.py
+++ import.py
@@ -1,6 +1,23 @@
 # 模块导入
 # import xx as xx
 
+# import time
+# print(time.localtime())
+
+# import time as t
+# print(t.localtime())
+
+# from time import localtime, time # 只导入想要的功能
+# print(time())
+# print(localtime())
+
+# from time import *    # 导入全部功能
+# print(time())
+
+# from input import test  # 导入自建模块 input
+# test()
+
+
 # 模块安装
 # pip3 install 模块名
 
